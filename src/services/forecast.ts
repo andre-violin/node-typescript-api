c10b78516ca70208d4b6bc917ee2a6c526b86fc9
André Violin <andre.violin@ifms.edu.br>
2022-09-27 23:52:26 -0400

    feat: add group Forcast by time logic

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -17,12 +17,17 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -32,12 +37,28 @@ export class Forecast {
           lng: beach.lng,
           name: beach.name,
           position: beach.position,
-          rating: 1,
+          rating: 1, // need to be implemented
         },
         ...e,
       }));
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return forecastByTime;
   }
 }
