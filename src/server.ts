9a5e632587225f184ccbe13f60ada436abdf82c5
André Violin <andre.violin@ifms.edu.br>
2022-09-29 17:46:09 -0400

    feat: add beaches controller and tests in green state

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,21 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
+
 export class SetupServer extends Server {
+  /**
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
   constructor(private port = 3000) {
     super();
   }
 
+  /**
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
   public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
@@ -17,11 +27,13 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.setupControllers();
   }
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   private async databaseSetup(): Promise<void> {
